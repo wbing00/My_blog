48b5b27eb86040486c04225babaf5716215677c1
wbing <3177715628@qq.com>
2025-05-10 20:54:39 +0800

    修改了navbar

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -6,7 +6,43 @@ export const navbar = defineNavbarConfig([
   { text: '标签', link: '/blog/tags/' },
   { text: '归档', link: '/blog/archives/' },
   {
-    text: '笔记',
-    items: [{ text: '示例', link: '/notes/demo/README.md' }]
+    text: 'Living Island',
+    items: [
+      { text: 'Become a Power Woman', link: '/notes/Living Island/Become a Power Woman/README.md' },
+    ]
   },
+
+  {
+    text: 'Skill Island',
+    items: [
+      { text: 'Jazz', link: '/notes/Skill Island/Jazz/README.md'},
+      { text: 'Used to be a debater', link: '/notes/Skill Island/Used to be a debater/README.md'},
+      { text: 'I love hosting', link: '/notes/Skill Island/I love hosting/README.md'},
+      { text: 'Video clip', link: '/notes/Skill Island/Video clip/README.md'},
+    ]
+  },
+
+  {
+    text: 'Growth Island',
+    items: [
+      { text: 'It is very messy here', link: '/notes/Growth Island/It is very messy here/README.md' },
+    ]
+  },
+
+  {
+    text: 'Working Island',
+    items: [
+      { text: 'I want to be a PM', link: '/notes/Working Island/I want to be a PM/README.md'},
+    ]
+  },
+
+  {
+    text: 'Photo Island',
+    items: [
+      { text: 'Yes,it is me', link: '/notes/Photo Island/Yes,it is me/README.md' },
+      { text: 'What I am drinking every day', link: '/notes/Photo Island/What I am drinking every day/README.md' },
+      { text: 'Photo Diary', link: '/notes/Photo Island/Photo Diary/README.md' },
+    ]
+  },
+
 ])
--- docs/README.md
+++ docs/README.md
@@ -7,9 +7,9 @@ config:
     full: true
     background: tint-plate
     hero:
-      name: Theme Plume
-      tagline: VuePress Next Theme
-      text: 一个简约的，功能丰富的 vuepress 文档&博客 主题
+      name: Through Her Eyes
+      # tagline: VuePress Next Theme
+      # text: 一个简约的，功能丰富的 vuepress 文档&博客 主题
       actions:
         -
           theme: brand
