2654bd5d569efeb8d33d4843664e5d686ec6b0d0
wbing <3177715628@qq.com>
2025-05-12 19:35:40 +0800

    saidebar测试

--- docs/.vuepress/notes/Drinking.ts
+++ docs/.vuepress/notes/Drinking.ts
@@ -1,13 +1,13 @@
 import { defineNoteConfig } from 'vuepress-theme-plume'
 
 export default defineNoteConfig({
-  dir: 'Photo Island',
-  link: '/What I am drinking every day/STARBUCKS/',
+  dir: 'Photo Island/What I am drinking every day/',
+  link: '/Drinking/',
   sidebar: [
     'README.md',
     {
       text: 'Coffee',
-      prefix: 'STARBUCKS', 
+      prefix: 'STARTBUCKS/', 
       items: [
         'STARBUCKS.md',
       ]
